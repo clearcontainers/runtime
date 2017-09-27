clearcontainers/runtime

commit ffe6ccfc9047245c0d6193738b00973f2ee5460c

build: Generalise ".in" file rule
Make the rule that generates the configuration file
more general to allow other files to be auto-generated.

Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

Makefile
@@ -123,7 +123,6 @@ COMMIT := $(if $(shell git status --porcelain --untracked-files=no),${COMMIT_NO}
 
 CONFIG_FILE = configuration.toml
 CONFIG = config/$(CONFIG_FILE)
-CONFIG_IN = $(CONFIG).in
 
 DESTTARGET := $(abspath $(DESTBINDIR)/$(TARGET))
 
@@ -249,12 +248,12 @@ endef
 export GENERATED_CODE
 
 
-GENERATED_FILES += config-generated.go
+GENERATED_GO_FILES += config-generated.go
 
 config-generated.go: Makefile VERSION
 	$(QUIET_GENERATE)echo "$$GENERATED_CODE" >$@
 
-$(TARGET): $(EXTRA_DEPS) $(SOURCES) $(GENERATED_FILES) Makefile | show-summary
+$(TARGET): $(EXTRA_DEPS) $(SOURCES) $(GENERATED_GO_FILES) $(GENERATED_FILES) Makefile | show-summary
 	$(QUIET_BUILD)go build -i -o $@ .
 
 pause: pause/pause.go
@@ -276,8 +275,12 @@ pause: pause/pause.go
 $(TARGET).coverage: $(SOURCES) $(GENERATED_FILES) Makefile
 	$(QUIET_TEST)go test -o $@ -covermode count
 
-$(CONFIG): $(CONFIG_IN) $(GENERATED_FILES)
+GENERATED_FILES += $(CONFIG)
+
+$(GENERATED_FILES): %: %.in Makefile VERSION
 	$(QUIET_CONFIG)$(SED) \
+		-e "s|@COMMIT@|$(COMMIT)|g" \
+		-e "s|@VERSION@|$(VERSION)|g" \
 		-e "s|@CONFIG_IN@|$(CONFIG_IN)|g" \
 		-e "s|@IMAGEPATH@|$(IMAGEPATH)|g" \
 		-e "s|@KERNELPATH@|$(KERNELPATH)|g" \
@@ -326,7 +329,7 @@ install-scripts:
 	$(QUIET_INST)install --mode 0755 -D $(SCRIPTS) $(SCRIPTS_DIR)
 
 clean:
-	$(QUIET_CLEAN)rm -f $(TARGET) $(CONFIG) $(GENERATED_FILES)
+	$(QUIET_CLEAN)rm -f $(TARGET) $(CONFIG) $(GENERATED_GO_FILES) $(GENERATED_FILES)
 	$(QUIET_CLEAN)rm -f pause/pause
 
 show-usage: show-header

commit 6ec4ecd2366e86ef40a2a75e8e44f0d7b99e3ba8

build: Fix config file warning message
Commit ffe6ccfc9047245c0d6193738b00973f2ee5460c inadvertently removed the
`CONFIG_IN` variable used by the build to generate part of the warning
message in the auto-generated config file.

Re-added to fix it.

Fixes #635.

Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

Makefile
@@ -123,6 +123,7 @@ COMMIT := $(if $(shell git status --porcelain --untracked-files=no),${COMMIT_NO}
 
 CONFIG_FILE = configuration.toml
 CONFIG = config/$(CONFIG_FILE)
+CONFIG_IN = $(CONFIG).in
 
 DESTTARGET := $(abspath $(DESTBINDIR)/$(TARGET))
 